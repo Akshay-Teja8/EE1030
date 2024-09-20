93fec0ad44f4cd03dbefc8f923a70749a5f1ba4c
Akshay-Teja8 <akshaytejakat@gmail.com>
2024-09-20 21:45:42 +0530

    file

--- Assignment3/Code/code.py
+++ Assignment3/Code/code.py
@@ -6,10 +6,6 @@ import subprocess
 result = subprocess.run(['./code'],stdout = subprocess.PIPE,text=True)
 output = result.stdout.strip().split('\n')
 
-#P = np.array([-2, 3, 5])
-#Q = np.array([1, 2, 3])
-#R = np.array([7, 0, -1])
-
 P = np.fromstring(output[0].replace('P ',''),sep=' ')
 Q = np.fromstring(output[1].replace('Q ',''),sep=' ')
 R = np.fromstring(output[2].replace('R ',''),sep=' ')
