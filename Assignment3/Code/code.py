d9ac9cbe6847b8b6f7c2e02aa31dc8bb98404bef
Akshay-Teja8 <akshaytejakat@gmail.com>
2024-09-20 14:39:40 +0530

    file

--- Assignment3/Code/code.py
+++ Assignment3/Code/code.py
@@ -20,13 +20,13 @@ for line in output:
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(*P, color='r', label='P(-2, 3, 5)')
-ax.scatter(*Q, color='g', label='Q(1, 2, 3)')
-ax.scatter(*R, color='b', label='R(7, 0, -1)')
+ax.scatter(*P, color='r', label='P')
+ax.scatter(*Q, color='g', label='Q')
+ax.scatter(*R, color='b', label='R')
 
-ax.text(*P,'P(-2, 3, 5)',color='black', ha='left')
-ax.text(*Q,'Q(1, 2, 3)',color='black', ha='left')
-ax.text(*R,'R(7, 0, -1)',color='black', ha='left')
+ax.text(P[0]+0.2,P[1],P[2],'P',fontsize=15,color='black', ha='left')
+ax.text(Q[0]+0.2,Q[1],Q[2],'Q',fontsize=15,color='black', ha='left')
+ax.text(R[0]+0.2,R[1],R[2],'R',fontsize=15,color='black', ha='left')
 
 ax.plot(x_values,y_values,z_values,color='k',label='Line through P,Q,R')
 
@@ -36,4 +36,4 @@ ax.set_zlabel('Z')
 
 plt.title('Points P, Q and R')
 plt.grid(True)
-plt.savefig('/home/akshay-teja-kondi/gvv/Assignment3/fig/fig.png')
+plt.savefig('/fig/fig.png')
